9dfe35428483dea7f8146bb2d74a90fd764ff818
unknown <sanjeetkumarit@outlook.com>
2025-02-26 15:04:03 +0530

    new resource s3 bucket

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -1,12 +1,16 @@
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 
 export class PhotosStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new Bucket(this, 'PhotosBucket',{
-         bucketName: 'photosbucket-234kgjgj5'
+   const myBucket= new Bucket(this, 'PhotosBucket2',{
+         bucketName: 'photosbucket-234kgjgjffsanjeet5'
     });
+    //if PhotosBucket2 as construct name/Logical ID
+    //create new resource, and delete old one
+
+    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket234567');
   }
 }
