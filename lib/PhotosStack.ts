5e35d6377883f5e830a6fbf691f6a8a64a4d3626
unknown <sanjeetkumarit@outlook.com>
2025-02-26 15:38:47 +0530

    CF instrinsic functions

--- lib/PhotosStack.ts
+++ lib/PhotosStack.ts
@@ -1,16 +1,23 @@
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { Fn } from 'aws-cdk-lib';
 
 export class PhotosStack extends cdk.Stack {
+    private stackSuffix:string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-   const myBucket= new Bucket(this, 'PhotosBucket2',{
-         bucketName: 'photosbucket-234kgjgjffsanjeet5'
+    this.initializeSuffix();
+     new Bucket(this, 'PhotosBucket2',{
+         bucketName: `photos-bucket-${this.stackSuffix}`,
     });
     //if PhotosBucket2 as construct name/Logical ID
     //create new resource, and delete old one
-
-    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket234567');
+   
+    //(myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket234567');
   }
+  private initializeSuffix(){
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId));
+ }
 }
