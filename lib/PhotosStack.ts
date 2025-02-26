f3e5562e3ddad933ccabbe5f2349cb288b7b0b85
unknown <sanjeetkumarit@outlook.com>
2025-02-26 14:33:56 +0530

    new cdk stack

---
+++ lib/PhotosStack.ts
@@ -0,0 +1,10 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+export class PhotosStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    new Bucket(this, 'PhotosBucket');
+  }
+}
