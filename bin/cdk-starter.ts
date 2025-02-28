243a861d842eec908eb496488cc7063dd522bc63
unknown <sanjeetkumarit@outlook.com>
2025-02-28 09:31:13 +0530

    CDK Aspects visitor pattern

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,8 +3,13 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStarterStack } from '../lib/cdk-starter-stack';
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
+
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBucketArn: photosStack.photosBucketArn
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
